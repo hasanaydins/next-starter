70b3f5ad58697dc901a31b075ed68dc8c0a8bf21
Hasan <hasanaydins@gmail.com>
2020-03-03 00:47:32 +0300

    method 1 added a Global Stylesheet

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,13 @@
 import Navigation from './navigation'
-import Head from "next/head";
+import Head from 'next/head'
 
 function Layout({ children }) {
   return (
     <div>
-        <Head>
-            <title>Hasan Aydin</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+      <Head>
+        <title>Hasan Aydin</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navigation />
       <main>{children}</main>
       <footer>hasan aydin. 2020</footer>
